a13a9102afd00128b6fa9f160c9c96a11ceb58cf
DilanLivera <DilanLivera@users.noreply.github.com>
2019-06-17 09:43:26 +1000

    change tooltip styling

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
               .attr("data-education", education)
               .style("opacity", 1)
               .style("left", `${d3.event.x - tooltip.node().offsetWidth/2}px`)
-              .style("top", `${d3.event.y - tooltip.node().offsetHeight + 20}px`)
+              .style("top", `${d3.event.y - tooltip.node().offsetHeight + 100}px`)
               .html(`
                 <p>${county} - ${state}</p>
                 <p>${education}</p>
