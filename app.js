3dc80d2e2cb8010ef36e4beda7f2d221750e91ce
DilanLivera <DilanLivera@users.noreply.github.com>
2019-06-17 12:15:21 +1000

    update tooltip styling

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ d3.select("#source")
           .attr("data-education", education)
           .style("opacity", 1)
           .style("left", `${d3.event.x - tooltip.node().offsetWidth/2}px`)
-          .style("top", `${d3.event.y - tooltip.node().offsetHeight + 100}px`)
+          .style("top", `${d3.event.y - 100}px`)
           .html(`
             <p>${county} - ${state}</p>
             <p>${education}</p>
