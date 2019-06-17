11b25b26600d98549e4e18451fddda3faa14381d
DilanLivera <DilanLivera@users.noreply.github.com>
2019-06-17 12:14:52 +1000

    add source

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const countyDataURL = " https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json";
   const title = "United States Educational Attainment";
   const description = "Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)";
-  const Source = "https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx";
-  const width = "1000";
-  const height = "650";
+  const source = "USDA Economic Research Service";
+  const SourceLink = "https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx";
+  const width = 1000;
+  const height = 650;
 
   //svg setup
   let svg = d3.select("svg")
               .attr("width", width)
               .attr("height", height)
-              .classed("map", true);
+              .classed("svg", true);
 
   //add a title
   d3.select("#title")
@@ -20,7 +21,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   d3.select("#description")
       .classed("description", true)
-    .text(description);    
+    .text(description);
+  
+d3.select("#source")
+    .attr("transform", `translateY(-50)`)
+  .append("p")
+    .text("Source: ")
+    .style("font-weight", "600")
+  .append("a")
+    .attr("href", SourceLink)
+    .text(source)
+    .style("text-decoration","none")
+    .style("font-weight", "100")
 
   d3.queue()
     .defer(d3.json, countyDataURL)
@@ -38,7 +50,6 @@ document.addEventListener("DOMContentLoaded", function() {
       let path = d3.geoPath();
 
       //setup color scales
-      let dataArr = geoData.map(d => d.countyData.bachelorsOrHigher);
       let minPoint = d3.min(geoData, d => d.countyData.bachelorsOrHigher)
       let maxPoint = d3.max(geoData, d => d.countyData.bachelorsOrHigher)
       let middlePoint = (maxPoint-minPoint)/8;
@@ -90,6 +101,7 @@ document.addEventListener("DOMContentLoaded", function() {
       
       svg
         .append("g")
+          .attr("id", "map")
         .selectAll(".county")
         .data(geoData)
         .enter()          
@@ -104,30 +116,30 @@ document.addEventListener("DOMContentLoaded", function() {
         .on("mouseout", hideTooltip)    
         .on("touchend", hideTooltip);          
 
-          function showTooltip(d) {
-            let fips = d.countyData.fips;
-            let education = d.countyData.bachelorsOrHigher;
-            let state = d.countyData.state;
-            let county = d.countyData.area_name;
-              
-            d3.select(this).classed("highlight", true);
-  
-            tooltip
-              .attr("data-fips", fips)
-              .attr("data-education", education)
-              .style("opacity", 1)
-              .style("left", `${d3.event.x - tooltip.node().offsetWidth/2}px`)
-              .style("top", `${d3.event.y - tooltip.node().offsetHeight + 100}px`)
-              .html(`
-                <p>${county} - ${state}</p>
-                <p>${education}</p>
-              `);          
-          }
-    
-          function hideTooltip() {
-            d3.select(this).classed("highlight", false);
-            tooltip
-              .style("opacity", 0);
-          }           
+      function showTooltip(d) {
+        let fips = d.countyData.fips;
+        let education = d.countyData.bachelorsOrHigher;
+        let state = d.countyData.state;
+        let county = d.countyData.area_name;
+          
+        d3.select(this).classed("highlight", true);
+
+        tooltip
+          .attr("data-fips", fips)
+          .attr("data-education", education)
+          .style("opacity", 1)
+          .style("left", `${d3.event.x - tooltip.node().offsetWidth/2}px`)
+          .style("top", `${d3.event.y - tooltip.node().offsetHeight + 100}px`)
+          .html(`
+            <p>${county} - ${state}</p>
+            <p>${education}</p>
+          `);          
+      }
+
+      function hideTooltip() {
+        d3.select(this).classed("highlight", false);
+        tooltip
+          .style("opacity", 0);
+      }           
     });
 });
